e254be23db89c6ec066afc350eaf97d319a82b84
Nico <nico@quickleft.com>
2010-10-15 15:45:35 -0600

    Release => Version: 1.0.0

---
+++ jquery.ql_select_puncher-1.0.0.js
@@ -0,0 +1,177 @@
+/*!
+ * Quickleft Select Puncher
+ * http://quickleft.com/
+ * Written by Nico Valencia
+ *
+ * Copyright 2010, Quick Left
+ * Licensed under the MIT license.
+ * ./license.txt
+ *
+ * git@github.com:quickleft/select_puncher.git
+ *
+ */
+(function($) {
+  jQuery.fn.select_puncher = function(options)
+  {
+    
+    var s_set = jQuery.extend({
+      p_vertical: 'bottom',
+      p_horizontal: 'right',
+      animation: 'fade',
+      animation_speed: 200
+    }, options);
+    
+    return this.each(
+      function() {
+        
+        //disable IE <= 7
+        if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
+          var ieversion = new Number(RegExp.$1);
+          if (ieversion <= 7) {
+            $('select').css({'display':'block'});
+            return false;
+          }
+        }
+        
+        //disable iDevices
+        if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+          $('select').css({'display':'block'});
+          return false;
+        }
+
+        var $this = $(this),
+            $s_id = '',
+            $s_name = $this.attr('name'),
+            $list = '',
+            $btn = '',
+            $selected = null,
+            $tagName = $this[0].tagName;
+            
+        //only SELECT tags
+        if ($tagName !== "SELECT") return false;
+        
+        //unique ID for select
+        $this.attr('id') ? $s_id = $this.attr('id') : $s_id = $this.attr('name').replace(/\[/g, '_').replace(/\]/g,'');
+        
+        /**
+         * BUILD LIST
+         * - value || html
+         * - # ? prepend 'val:'
+         */
+        $this.find('option').each(function(i, v) {
+          
+          var obj = $(v),
+              value = obj.val(),
+              html = obj.html();
+          
+          //prepend 'val:' to numbers
+          if (value * 1 == value) value = 'val:' + value;
+          
+          //add list item
+          $list += '<li><span name="' + value + '">' + html + '</span></li>';
+          
+          //set selected option
+          if (obj.attr('selected') == true) $selected = html;
+          
+        });
+        
+        //attach list
+        $this.after('<ul id="' + $s_id + '_list" class="s_p_list">' + $list + '</ul>');
+        $list = $('#' + $s_id + '_list');
+        
+        //remove select - add button
+        $this.remove();
+        $list.before('<div id="' + $s_id + '_btn" class="s_p_btn"><span class="content">' + $s_id + '</span><span class="arrow">&#8226;</span></div>');
+        $list.after('<input id="' + $s_id + '" name="' + $s_name + '" type="hidden" value="" />');
+        
+        //define s_p_btn
+        $btn = $('#' + $s_id + '_btn');
+
+        /**
+         * BIND CLICK: s_p_button
+         * - position list
+         * - close others
+         * - animate
+         */
+        $btn.bind('click', function(e) {
+          
+          //disable body 'click'
+          e.stopPropagation();
+          
+          var btn_pos = $btn.position(),
+              list_pos = $list.position(),
+              s_index = $('.s_p_btn').index($(this));
+          
+          //set pos of list
+          s_set.p_vertical == 'top' ? list_pos.top = btn_pos.top - $list.outerHeight() : list_pos.top = btn_pos.top + $btn.outerHeight();
+          s_set.p_horizontal == 'left' ? list_pos.left = btn_pos.left : list_pos.left = btn_pos.left - ($list.outerWidth() - $btn.outerWidth());
+          $list.css({top:list_pos.top,left:list_pos.left});
+          
+          //close other open lists
+          $('.s_p_btn').each(function(index) {
+            if ($(this).hasClass('down') && index != s_index) {
+              $(this).removeClass('down');
+              $('.s_p_btn:eq(' + $('.s_p_btn').index($(this)) + ')').trigger('click');
+            }
+          });
+          
+          //animate
+          if ($list.is(':visible')) {
+            switch (s_set.animation) {
+              case 'fade':
+                $list.stop().fadeOut(s_set.animation_speed, function() {
+                  $(this).css({'opacity':1})
+                });
+                break;
+              case 'slide':
+                $list.slideUp(s_set.animation_speed);
+                break;
+              default:
+                $list.hide();
+            }
+            $(this).removeClass('down');
+          } else {
+            switch (s_set.animation) {
+              case 'fade':
+                $list.stop().fadeIn(s_set.animation_speed, function() {
+                  $(this).css({'opacity':1});
+                });
+                break;
+              case 'slide':
+                $list.slideDown(s_set.animation_speed);
+                break;
+              default:
+                $list.show();
+            }
+            $(this).addClass('down');
+          }
+          
+        });
+        
+        /**
+         * BIND CLICK: s_p_list span
+         * - set hidden input value
+         * - set s_p_btn text
+         * - close list
+         */
+        $list.find('span').bind('click', function() {
+          
+          var in_val = '';
+          
+          //split value if number, and set input
+          in_val = $(this).attr('name');
+          if (in_val.split(':')[0] == 'val') in_val = in_val.split(':')[1];
+          $('#' + $s_id).val(in_val);
+          
+          //set button text and close list
+          $('#' + $s_id + '_btn .content').html( $(this).html() ).trigger('click');
+          
+        });
+        
+        //set default selected
+        $('#' + $s_id + '_btn .content').html($selected);
+
+    });
+      
+  };
+})(jQuery);
--- thepower.js
+++ thepower.js
@@ -1,229 +1,52 @@
 /*!
- * QL Canvas
- * Written by Nico Valencia
+ * Quickleft Select Puncher
  * http://quickleft.com/
+ * Written by Nico Valencia
  *
- * Copyright 2010, Quick Left, Inc.
+ * Copyright 2010, Quick Left
+ * Licensed under the MIT license.
+ * ./license.txt
  *
- * Date: 14 July 2010
+ * git@github.com:quickleft/select_puncher.git
+ *
  */
-
 (function(){
 	
-	//this holds our interval
-	var $anim_obj;
-	
-	//this is our 2d canvas element
-	var $context;
-	
-	//this is our iterator array
-	var $i = new Array();
-	$i['draw_count'] = 0;
-	$i['last_second'] = 0;
-	$i['fps'] = 0;
-	
-	//this is our interval speed
-	var $interval = 1;
-	
-	//these are our canvas params
-	var $canvas = {
-		w: 800,
-		h: 400
-	}
-	
-	var CANVAS = {
+	var SP = {
 		init: function(){
+
+      //example 1
+      $('#myform_drinks').select_puncher({
+        p_vertical: 'bottom',
+        p_horizontal: 'right',
+        animation: 'default'
+      });
+      
+      //example 2
+      $('#myform_numbers').select_puncher({
+        p_vertical: 'bottom',
+        p_horizontal: 'right',
+        animation: 'default'
+      });
+      
+      //example 3
+      $('#myform_colors').select_puncher({
+        p_vertical: 'top',
+        p_horizontal: 'left',
+        animation: 'fade',
+        animation_speed: 500
+      });
+
+      //global close
+      $('body').bind('click',function(){
+        $('.s_p_btn').each(function(){
+          if($(this).hasClass('down')) $(this).removeClass('down').trigger('click');
+        });
+      });
 			
-			CANVAS.msg('Welcome to QL CANVAS!');
-			CANVAS.bindEvents();
-			
-		},
-		bindEvents: function(){
-			
-			//actions
-			$('.action').bind('click',function(){
-				CANVAS.draw.init($(this).attr('data-action'));
-				return false;
-			});
-			$('#stop a').bind('click',function(){
-				CANVAS.msg('Stopped.')
-				clearInterval($anim_obj);
-				return false;
-			});
-			$('#clear a').bind('click',function(){
-				CANVAS.msg('Clearing...')
-				CANVAS.draw.clearCanvas();
-				return false;
-			});
-			$('#interval_go').bind('click',function(){
-				CANVAS.changeInterval();
-			});
-			//messages
-			$('.flash_message').bind('mouseenter',function(){
-				CANVAS.flashMsg($(this).attr('data-message'));
-			});
-			$('.flash_message').bind('mouseleave',function(){
-				$('#msg').css({opacity:0});
-			});
-			//interval
-			$('#interval').bind('focus',function(){
-				$(this).val('');
-			});
-			
-		},
-		draw: {
-			init: function(action){
-				
-				CANVAS.msg('Drawing: '+action+'.');
-				
-				//reset canvas
-				clearInterval($anim_obj);
-				CANVAS.draw.clearCanvas();
-				
-				//initialize the canvas
-				$context = document.getElementById('ql_canvas');
-				$context = $context.getContext('2d');
-				
-				//reset our iterator
-				$i['draw_count'] = 0;
-				$i['last_second'] = 0;
-				$i['fps'] = 0;
-				
-				//call action
-				switch(action){
-					case 'circle':
-						$context.strokeStyle = "#0099ff";
-						$anim_obj = setInterval(CANVAS.draw.circle,$interval);
-						break;
-					case 'calculusTween':
-						$i['x'] = 0;
-						$i['y'] = 0;
-						$context.strokeStyle = "#000";
-						$anim_obj = setInterval(CANVAS.draw.calculusTween,$interval);
-						break;
-					case 'gradient':
-						$anim_obj = setInterval(CANVAS.draw.gradient,$interval);
-						break;
-					default:
-						break;
-				}
-				
-			},
-			circle: function(){
-				
-				//random location
-				var x = Math.floor(Math.random()*$canvas.w), y = Math.floor(Math.random()*$canvas.h), radius = Math.floor(Math.random()*200);
-				
-				//draw
-				$context.beginPath();
-				$context.arc(x, y, radius, 0, Math.PI * 2, false);
-				$context.closePath();
-				$context.stroke();
-				
-				//stats
-				CANVAS.updateStats();
-				
-			},
-			calculusTween: function(){
-				
-				CANVAS.draw.clearCanvas();
-				
-				//draw
-				$context.beginPath();
-				$context.arc($i['x'], $i['y'], ($canvas.h/10), 0, Math.PI * 2, false);
-				$context.closePath();
-				$context.stroke();
-				
-				//count
-				$i['x']++;
-				$i['y'] = Math.sin($i['x']/20)*($canvas.h/4) + ($canvas.h/2);
-				
-				//reset
-				if($i['x'] > $canvas.w + 30) $i['x'] = 0;
-				
-				//stats
-				CANVAS.updateStats();
-				
-			},
-			gradient: function(){
-				
-				//draw
-				var gradient = $context.createRadialGradient($canvas.w/2,$canvas.h/2,0,$canvas.w/2,$canvas.h/2,$canvas.w/2);
-				gradient.addColorStop(0, 'rgb('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')');
-				gradient.addColorStop(1, 'rgb('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')');
-				$context.fillStyle = gradient;
-				$context.fillRect(0, 0, 800, 400);
-				
-				//stats
-				CANVAS.updateStats();
-				
-			},
-			clearCanvas: function(){
-				
-				/* This is the Traditional Method
-				 * However, it is buggy in Chrome, so we just re-draw a clean canvas
-				 *
-				 *		var clear_this = document.getElementById('ql_canvas');
-				 *		clear_this.width = 800;
-				 *
-				 */
-				
-				if($context) $context.clearRect(0,0,$canvas.w,$canvas.h);
-				
-			}
-		},
-		changeInterval: function(){
-			
-			clearInterval($anim_obj);
-			
-			//set entered number
-			var interval_field = parseInt($('#interval').val());
-			if(isNaN(interval_field)){
-				$interval = 1;
-			} else {
-				$interval = interval_field;
-			}
-			
-			//reset canvas
-			CANVAS.draw.clearCanvas();
-			
-			//animate interval field
-			$('#interval').slideUp(300,function(){
-				$(this).val($interval);
-				$(this).slideDown(300);
-			});
-			
-		},
-		updateStats: function(){
-			
-			var right_now = new Date();
-			
-			//count
-			$i['draw_count']++;
-			$('#draw_count span').html($i['draw_count']);
-			
-			//fps
-			if(right_now.getSeconds() != $i['last_second']){
-				$i['last_second'] = right_now.getSeconds();
-				$('#fps span').html($i['fps']);
-				$i['fps'] = 0;
-			} else {
-				$i['fps']++;
-			}
-			
-			//date
-			$('#time').html(right_now.toGMTString());
-			
-		},
-		flashMsg: function(msg){
-			$('#msg').html(msg);
-			$('#msg').css({opacity:.9});
-		},
-		msg: function(msg){
-			if(window.console) console.log(msg);
 		}
-	}
+	};
 	
-	$(document).ready(CANVAS.init);
+	$(document).ready(SP.init);
 	
 })();
